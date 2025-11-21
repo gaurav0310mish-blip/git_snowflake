e5d06c8bdda5186b8b10fa4ea0698ac323284a84
gaurav0310mish-blip <gaurav0310mish@gmail.com>
2025-11-21 16:12:46 +0530

    Create 00_create_schema.sql

---
+++ sql/00_create_schema.sql
@@ -0,0 +1,13 @@
+-- Create schema/user if needed (optional – adjust for your environment)
+-- In many ADW setups, the user/schema already exists.
+-- This is just an example.
+
+-- CREATE USER adw_user IDENTIFIED BY "password"
+--   DEFAULT TABLESPACE data
+--   TEMPORARY TABLESPACE temp
+--   QUOTA UNLIMITED ON data;
+
+-- GRANT CONNECT, RESOURCE TO adw_user;
+-- GRANT DWROLE TO adw_user;
+
+-- Run this as ADMIN or a privileged user if you need to create the schema.

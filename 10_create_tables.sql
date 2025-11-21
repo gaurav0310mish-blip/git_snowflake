4456c5f71b118f28f0337f2000825e96117d800d
gaurav0310mish-blip <gaurav0310mish@gmail.com>
2025-11-21 16:08:36 +0530

    Delete 10_create_tables.sql

--- 10_create_tables.sql
+++
@@ -1,8 +0,0 @@
--- Connect as ADW_USER (or your target schema) before running
-
-CREATE TABLE IF NOT EXISTS MYTABLE (
-  ID            NUMBER,
-  NAME          VARCHAR2(200),
-  AMOUNT        NUMBER,
-  LOAD_TS       TIMESTAMP DEFAULT SYSTIMESTAMP
-);
